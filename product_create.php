2cb84e2939338f7309f13706ec530ae04e05b470
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-03-14 10:41:11 +0800

    edit

--- product_create.php
+++ product_create.php
@@ -18,7 +18,41 @@
 
         <!-- html form to create product will be here -->
 
-        <!-- PHP insert code will be here -->
+        <?php
+        if ($_POST) {
+            // include database connection
+            include 'config/database.php';
+            try {
+                // posted values
+                $name = htmlspecialchars(strip_tags($_POST['name']));
+                $description = htmlspecialchars(strip_tags($_POST['description']));
+                $price = htmlspecialchars(strip_tags($_POST['price']));
+                // insert query
+                $query = "INSERT INTO products SET name=:name, description=:description, price=:price, created=:created";
+                // prepare query for execution
+                $stmt = $con->prepare($query);
+                // bind the parameters
+                $stmt->bindParam(':name', $name);
+                $stmt->bindParam(':description', $description);
+                $stmt->bindParam(':price', $price);
+                // specify when this record was inserted to the database
+                $created = date('Y-m-d H:i:s');
+                $stmt->bindParam(':created', $created);
+                // Execute the query
+                if ($stmt->execute()) {
+                    echo "<div class='alert alert-success'>Record was saved.</div>";
+                } else {
+                    echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                }
+            }
+            // show error
+            catch (PDOException $exception) {
+                die('ERROR: ' . $exception->getMessage());
+            }
+        }
+        ?>
+
+
 
         <!-- html form here where the product information will be entered -->
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
