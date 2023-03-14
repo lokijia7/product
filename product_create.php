40cdc5842d052b1e9dece7030d8a2ee5cd7305db
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-03-14 20:55:00 +0800

    add date and price

--- product_create.php
+++ product_create.php
@@ -47,22 +47,38 @@
                 $name = htmlspecialchars(strip_tags($_POST['name']));
                 $description = htmlspecialchars(strip_tags($_POST['description']));
                 $price = htmlspecialchars(strip_tags($_POST['price']));
-                // insert query
-                $query = "INSERT INTO products SET name=:name, description=:description, price=:price, created=:created";
-                // prepare query for execution
-                $stmt = $con->prepare($query);
-                // bind the parameters
-                $stmt->bindParam(':name', $name);
-                $stmt->bindParam(':description', $description);
-                $stmt->bindParam(':price', $price);
-                // specify when this record was inserted to the database
-                $created = date('Y-m-d H:i:s');
-                $stmt->bindParam(':created', $created);
-                // Execute the query
-                if ($stmt->execute()) {
-                    echo "<div class='alert alert-success'>Record was saved.</div>";
+                $promo_price = htmlspecialchars(strip_tags($_POST['promo_price']));
+                $manufacture_date = htmlspecialchars(strip_tags($_POST['manufacture_date']));
+                $expiry_date = htmlspecialchars(strip_tags($_POST['expiry_date']));
+
+                // check if promotion price is less than original price
+                if ($promo_price >= $price) {
+                    echo "<div class='alert alert-danger'>Promotion price must be cheaper than original price.</div>";
+                }
+                // check if expiry date is later than manufacture date
+                else if ($expiry_date <= $manufacture_date) {
+                    echo "<div class='alert alert-danger'>Expiry date must be later than manufacture date.</div>";
                 } else {
-                    echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                    // insert query
+                    $query = "INSERT INTO products SET name=:name, description=:description, price=:price, created=:created";
+                    // prepare query for execution
+                    $stmt = $con->prepare($query);
+                    // bind the parameters
+                    $stmt->bindParam(':name', $name);
+                    $stmt->bindParam(':description', $description);
+                    $stmt->bindParam(':price', $price);
+                    $stmt->bindParam(':promo_price', $promo_price);
+                    $stmt->bindParam(':manufacture_date', $manufacture_date);
+                    $stmt->bindParam(':expiry_date', $expiry_date);
+                    // specify when this record was inserted to the database
+                    $created = date('Y-m-d H:i:s');
+                    $stmt->bindParam(':created', $created);
+                    // Execute the query
+                    if ($stmt->execute()) {
+                        echo "<div class='alert alert-success'>Record was saved.</div>";
+                    } else {
+                        echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                    }
                 }
             }
             // show error
@@ -90,11 +106,26 @@
                     <td><input type='text' name='price' class='form-control' /></td>
                 </tr>
                 <tr>
-                    <td></td>
-                    <td>
-                        <input type='submit' value='Save' class='btn btn-primary' />
-                        <a href='index.php' class='btn btn-danger'>Back to read products</a>
-                    </td>
+                <tr>
+                    <td>Promotion price</td>
+                    <td><input type='text' name='price' class='form-control' /></td>
+                </tr>
+                <tr>
+                <tr>
+                    <td>Manufacture Date</td>
+                    <td><input type='date' name='manufacture_date' class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Expiry Date</td>
+                    <td><input type='date' name='expiry_date' class='form-control' /></td>
+                </tr>
+
+
+                <td></td>
+                <td>
+                    <input type='submit' value='Save' class='btn btn-primary' />
+                    <a href='index.php' class='btn btn-danger'>Back to read products</a>
+                </td>
                 </tr>
             </table>
         </form>
