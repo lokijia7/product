1274914b44dbc74d2a6e2fcd4224c57ee16ca404
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-03-14 10:23:35 +0800

    edit

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+// used to connect to the database
+$host = "localhost";
+$db_name = "lokijia7";
+$username = "lokijia7";
+$password = "_[IGHaihS4tyLM@l";
+  
+try {
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+    echo "Connected successfully"; 
+}
+  
+// show error
+catch(PDOException $exception){
+    echo "Connection error: ".$exception->getMessage();
+}
