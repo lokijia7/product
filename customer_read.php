d608fa3b943063691bae5cd80b446a34f7494d1f
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-03-30 20:32:18 +0800

    add the table heading and contents

---
+++ customer_read.php
@@ -0,0 +1,119 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <!-- Latest compiled and minified Bootstrap CSS (Apply your Bootstrap here -->
+</head>
+
+<body>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+        <div class="container px-2 px-lg-3">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto py-4 py-lg-0">
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="product_create.php">Create Product</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="product_read.php">Products List</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="customer_create.php">Create Customer</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="customer_read.php">Customers List</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- container -->
+    <div class="container">
+        <div class="page-header">
+            <h1>Read Customers</h1>
+        </div>
+
+        <?php
+        // include database connection
+        include 'config/database.php';
+
+        // delete message prompt will be here
+
+        // select all data
+        $query = "SELECT * FROM customers";
+        $stmt = $con->prepare($query);
+        $stmt->execute();
+
+        // this is how to get number of rows returned
+        $num = $stmt->rowCount();
+
+        // link to create record form
+        echo "<a href='customer_create.php' class='btn btn-primary m-b-1em'>Create New Customer</a>";
+
+        //check if more than 0 record found
+        if ($num > 0) {
+
+            echo "<table class='table table-hover table-responsive table-bordered'>"; //start table
+
+            //creating our table heading
+            echo "<tr>";
+            echo "<th>ID</th>";
+            echo "<th>Username</th>";
+            echo "<th>First Name</th>";
+            echo "<th>Last Name</th>";
+            echo "<th>Password</th>";
+            echo "<th>Gender</th>";
+            echo "<th>Date Of Birth</th>";
+            echo "<th>Account Status</th>";
+            echo "<th>Action</th>";
+            echo "</tr>";
+
+            // retrieve our table contents
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                // extract row
+                // this will make $row['firstname'] to just $firstname only
+                extract($row);
+                // creating new table row per record
+                echo "<tr>";
+                echo "<td>{$id}</td>";
+                echo "<td>{$username}</td>";
+                echo "<td>{$fname}</td>";
+                echo "<td>{$lname}</td>";
+                echo "<td>{$pass}</td>";
+                echo "<td>{$gender}</td>";
+                echo "<td>{$date_of_birth}</td>";
+                echo "<td>{$account_status}</td>";
+                echo "<td>";
+                // read one record
+                echo "<a href='product_read_one.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
+
+                // we will use this links on next part of this post
+                echo "<a href='update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
+
+                // we will use this links on next part of this post
+                echo "<a href='#' onclick='delete_user({$id});'  class='btn btn-danger'>Delete</a>";
+                echo "</td>";
+                echo "</tr>";
+            }
+
+
+            // end table
+            echo "</table>";
+        }
+        // if no records found
+        else {
+            echo "<div class='alert alert-danger'>No records found.</div>";
+        }
+        ?>
+
+
+    </div> <!-- end .container -->
+
+    <!-- confirm delete record will be here -->
+
+</body>
+
+</html>
