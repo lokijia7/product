ade990039bddb320972137d446f6f30cd12b6224
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-03-31 14:33:59 +0800

    insert php

--- login.php
+++ login.php
@@ -10,6 +10,55 @@
 </head>
 
 <body>
+
+    <?php
+    include("database.php"); // include database connection file  
+    // start session
+    session_start();
+
+    // check if the login form is submitted
+    if (isset($_POST['login'])) {
+        // get the username/email and password from the form
+        $username_email = $_POST['username_email'];
+        $password = $_POST['password'];
+
+        // check if the username/email and password are not empty
+        if (!empty($username_email) && !empty($password)) {
+            // create a SQL query to check if the user exists and the password is correct
+            $query = "SELECT * FROM users WHERE (username = '$username_email' OR email = '$username_email') AND password = '$password'";
+            $result = mysqli_query($conn, $query);
+
+            // check if the query returned a result
+            if (mysqli_num_rows($result) == 1) {
+                // get the user data from the database
+                $user = mysqli_fetch_assoc($result);
+
+                // check if the user account is active
+                if ($user['status'] == 'active') {
+                    // set the user session variables
+                    $_SESSION['user_id'] = $user['id'];
+                    $_SESSION['username'] = $user['username'];
+                    $_SESSION['email'] = $user['email'];
+
+                    // redirect the user to the home page
+                    header('Location: home.php');
+                    exit;
+                } else {
+                    // show an error message if the user account is inactive
+                    $error_msg = 'Your account is inactive. Please contact the administrator.';
+                }
+            } else {
+                // show an error message if the username/email or password is incorrect
+                $error_msg = 'Invalid username/email or password.';
+            }
+        } else {
+            // show an error message if the username/email or password is empty
+            $error_msg = 'Please enter your username/email and password.';
+        }
+    }
+    ?>
+
+
     <div id="login">
         <h3 class="text-center text-white pt-5">Login form</h3>
         <div class="container">
