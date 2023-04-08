f613e748d253069fc70c2e6e90f0b845e8bdfe9a
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-04-08 23:49:16 +0800

    create category

--- category_read.php
+++ category_read.php
@@ -11,60 +11,79 @@
 
 <body>
     <?php include 'nav.php' ?>
-
     <!-- container -->
     <div class="container">
         <div class="page-header">
-            <h1>Create Category</h1>
+            <h1>Read Categories</h1>
         </div>
 
         <?php
         // include database connection
         include 'config/database.php';
-        // Get the selected category from the URL parameter
-        $category_name = $_GET["category_name"];
 
-        // Define the SQL query
-        $query = "SELECT * FROM products WHERE category_name = '$category_name'";
+        // select all data
+        $query = "SELECT * FROM product_category";
+        $stmt = $con->prepare($query);
+        $stmt->execute();
 
-        // Execute the query and store the results in a variable
-        $result = mysqli_query($conn, $query);
+        // this is how to get number of rows returned
+        $num = $stmt->rowCount();
 
-        // Close the database connection
-        mysqli_close($conn);
+        // link to create record form
+        echo "<a href='category_create.php' class='btn btn-primary m-b-1em'>Create New Category</a>";
+
+        //check if more than 0 record found
+        if ($num > 0) {
+
+            echo "<table class='table table-hover table-responsive table-bordered'>"; //start table
+
+            //creating our table heading
+            echo "<tr>";
+            echo "<th>Category ID</th>";
+            echo "<th>Category Name</th>";
+            echo "<th>Description</th>";
+            echo "<th>Status</th>";
+            echo "</tr>";
+
+            // retrieve our table contents
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                // extract row
+                // this will make $row['firstname'] to just $firstname only
+                extract($row);
+                // creating new table row per record
+                echo "<tr>";
+                echo "<td>{$category_id}</td>";
+                echo "<td>{$category_name}</td>";
+                echo "<td>{$category_description}</td>";
+                echo "<td>{$category_status}</td>";
+                echo "<td>";
+                // read one record
+                echo "<a href='category_read_one.php?id={$id}' class='btn btn-info m-r-1em'>Read</a>";
+
+                // we will use this links on next part of this post
+                echo "<a href='update.php?id={$id}' class='btn btn-primary m-r-1em'>Edit</a>";
+
+                // we will use this links on next part of this post
+                echo "<a href='#' onclick='delete_category({$id});'  class='btn btn-danger'>Delete</a>";
+                echo "</td>";
+                echo "</tr>";
+            }
+
+
+            // end table
+            echo "</table>";
+        }
+        // if no records found
+        else {
+            echo "<div class='alert alert-danger'>No records found.</div>";
+        }
         ?>
 
-        <!DOCTYPE html>
-        <html>
 
-        <head>
-            <title>Products</title>
-        </head>
+    </div> <!-- end .container -->
 
-        <body>
-            <h1><?php echo $category; ?></h1>
-            <table>
-                <tr>
-                    <th>Name</th>
-                    <th>Description</th>
-                    <th>Price</th>
-                </tr>
-                <?php while ($row = mysqli_fetch_assoc($result)) { ?>
-                    <tr>
-                        <td><?php echo $row["name"]; ?></td>
-                        <td><?php echo $row["description"]; ?></td>
-                        <td><?php echo $row["price"]; ?></td>
-                    </tr>
-                <?php } ?>
-            </table>
-        </body>
+    <!-- confirm delete record will be here -->
 
-        </html>
-        ?>
-
-
-        <!-- end .container -->
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- nav.php
+++ nav.php
@@ -1,18 +1,3 @@
-<!-- Navigation-->
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
-// include database connection
-include('config/database.php');
-
-$base = basename($_SERVER['PHP_SELF'], '.php');
-
-// Fetch categories from the database
-$stmt = $con->query('SELECT * FROM product_category');
-$categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
-?>
-
 <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
     <div class="container px-2 px-lg-3">
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
@@ -22,28 +7,40 @@ $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto py-4 py-lg-0">
                 <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="home.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="product_create.php">Create Product</a></li>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="product_read.php">Products List</a></li>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="category_create.php">Create Category</a></li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle px-lg-3 py-3 py-lg-4" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Category
+                    <a class="nav-link px-lg-3 py-3 py-lg-4 dropdown-toggle" href="#" id="navbarDropdownProduct" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Product
                     </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <?php foreach ($categories as $product_category) { ?>
-                            <li><a class="dropdown-item" href="#"><?php echo $product_category['category_name']; ?></a></li>
-                        <?php } ?>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownProduct">
+                        <li><a class="dropdown-item" href="product_create.php">Create Product</a></li>
+                        <li><a class="dropdown-item" href="product_read.php">Product List</a></li>
                     </ul>
                 </li>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="customer_create.php">Create Customer</a></li>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="customer_read.php">Customers List</a></li>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.php">Contact Us</a>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="login.php">Log out</a>
+                <li class="nav-item dropdown">
+                    <a class="nav-link px-lg-3 py-3 py-lg-4 dropdown-toggle" href="#" id="navbarDropdownCategory" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Category
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownCategory">
+                        <li><a class="dropdown-item" href="category_create.php">Create Category</a></li>
+                        <li><a class="dropdown-item" href="category_read.php">Category List</a></li>
+                    </ul>
                 </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link px-lg-3 py-3 py-lg-4 dropdown-toggle" href="#" id="navbarDropdownCustomer" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Customer
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownCustomer">
+                        <li><a class="dropdown-item" href="customer_create.php">Create Customer</a></li>
+                        <li><a class="dropdown-item" href="customer_read.php">Customers List</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.php">Contact Us</a></li>
+                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="login.php">Log out</a></li>
             </ul>
         </div>
     </div>
 </nav>
+
 <style>
     .dropdown:hover .dropdown-menu {
         display: block;
