c371ab524ae4a66a4cb271ddc52f557aca91a875
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-04-13 23:22:20 +0800

    add style

--- home.php
+++ home.php
@@ -18,6 +18,7 @@ if (!isset($_SESSION["username"])) {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <link rel="stylesheet" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <!-- Latest compiled and minified Bootstrap CSS (Apply your Bootstrap here -->
 </head>
--- nav.php
+++ nav.php
@@ -1,45 +1,69 @@
-<nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
-    <div class="container px-2 px-lg-3">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-            Menu
-            <i class="fas fa-bars"></i>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-            <ul class="navbar-nav ms-auto py-4 py-lg-0">
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="home.php">Home</a></li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link px-lg-3 py-3 py-lg-4 dropdown-toggle" href="#" id="navbarDropdownProduct" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Product
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownProduct">
-                        <li><a class="dropdown-item" href="product_create.php">Create Product</a></li>
-                        <li><a class="dropdown-item" href="product_read.php">Product List</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link px-lg-3 py-3 py-lg-4 dropdown-toggle" href="#" id="navbarDropdownCategory" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Category
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownCategory">
-                        <li><a class="dropdown-item" href="category_create.php">Create Category</a></li>
-                        <li><a class="dropdown-item" href="category_read.php">Category List</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link px-lg-3 py-3 py-lg-4 dropdown-toggle" href="#" id="navbarDropdownCustomer" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Customer
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownCustomer">
-                        <li><a class="dropdown-item" href="customer_create.php">Create Customer</a></li>
-                        <li><a class="dropdown-item" href="customer_read.php">Customers List</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.php">Contact Us</a></li>
-                <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="logout.php" name="logout">Log out</a></li>
-            </ul>
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$currentPage = basename($_SERVER['PHP_SELF']);
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
+        <div class="container px-2 px-lg-3">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto py-4 py-lg-0">
+                    <li class="nav-item <?php if ($currentPage == 'home.php') {
+                                            echo 'active';
+                                        } ?>"><a class="nav-link px-lg-3 py-3 py-lg-4" href="home.php">Home</a></li>
+                    <li class="nav-item <?php if ($currentPage == 'product_create.php' || $currentPage == 'product_read.php') {
+                                            echo 'active';
+                                        } ?> dropdown">
+                        <a class="nav-link px-lg-3 py-3 py-lg-4 dropdown-toggle" href="#" id="navbarDropdownProduct" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Product
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownProduct">
+                            <li><a class="dropdown-item" href="product_create.php">Create Product</a></li>
+                            <li><a class="dropdown-item" href="product_read.php">Product List</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item <?php if ($currentPage == 'category_create.php' || $currentPage == 'category_read.php') {
+                                            echo 'active';
+                                        } ?> dropdown">
+                        <a class="nav-link px-lg-3 py-3 py-lg-4 dropdown-toggle" href="#" id="navbarDropdownProduct" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Category
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownProduct">
+                            <li><a class="dropdown-item" href="category_create.php">Create Category</a></li>
+                            <li><a class="dropdown-item" href="category_read.php">Category List</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item <?php if ($currentPage == 'customer_create.php' || $currentPage == 'customer_read.php') {
+                                            echo 'active';
+                                        } ?> dropdown">
+                        <a class="nav-link px-lg-3 py-3 py-lg-4 dropdown-toggle" href="#" id="navbarDropdownProduct" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Customer
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownProduct">
+                            <li><a class="dropdown-item" href="customer_create.php">Create customer</a></li>
+                            <li><a class="dropdown-item" href="customer_read.php">Customer List</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item <?php if ($currentPage == 'contact.php') {
+                                            echo 'active';
+                                        } ?>"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.php">Contact</a></li>
+                    <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="logout.php" name="logout">Log out</a></li>
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+
+</html>
 
 <style>
     .dropdown:hover .dropdown-menu {
