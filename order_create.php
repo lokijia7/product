d6de42f22130c17434e1363b921f044b0815b40f
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-04-20 23:57:24 +0800

    product_id is null

--- order_create.php
+++ order_create.php
@@ -44,21 +44,57 @@ if (!isset($_SESSION["username"])) {
             try {
                 // posted values
                 if (isset($_POST['username'])) $username = $_POST['username'];
-                if (isset($_POST['name'])) $name = $_POST['name'];
-
-
+                if (isset($_POST['product1_name'])) {
+                    $product1 = $_POST['product1_name'];
+                }
+                if (isset($_POST['product2_name'])) {
+                    $product2 = $_POST['product2_name'];
+                }
+                if (isset($_POST['product3_name'])) {
+                    $product3 = $_POST['product3_name'];
+                }
+                if (isset($_POST['product1_quantity'])) {
+                    $quantity1 = $_POST['product1_quantity'];
+                }
+                if (isset($_POST['product2_quantity'])) {
+                    $quantity2 = $_POST['product2_quantity'];
+                }
+                if (isset($_POST['product3_quantity'])) {
+                    $quantity3 = $_POST['product3_quantity'];
+                }
                 $flag = false;
 
 
 
-                // Check if any field is empty
+                // Check if any product field is empty
                 if (empty($username)) {
-                    $name_err = "Please fill out the Name field.";
+                    $username_err = "Please fill out the name field.";
                     $flag = true;
                 }
-                // Check if any field is empty
-                if (empty($name)) {
-                    $product_name_err = "Please fill out the Product field.";
+                if (empty($product1)) {
+                    $product1_name_err = "Please fill out the Product field.";
+                    $flag = true;
+                }
+                if (empty($product2)) {
+                    $product2_name_err = "Please fill out the Product field.";
+                    $flag = true;
+                }
+                if (empty($product3)) {
+                    $product3_name_err = "Please fill out the Product field.";
+                    $flag = true;
+                }
+
+                // Check if any quantity field is empty
+                if (empty($quantity1)) {
+                    $product1_quantity_err = "Please fill out the Quantity field.";
+                    $flag = true;
+                }
+                if (empty($quantity2)) {
+                    $product2_quantity_err = "Please fill out the Quantity field.";
+                    $flag = true;
+                }
+                if (empty($quantity3)) {
+                    $product3_quantity_err = "Please fill out the Quantity field.";
                     $flag = true;
                 }
 
@@ -69,22 +105,95 @@ if (!isset($_SESSION["username"])) {
                 if ($flag == false) {
 
                     // insert query
-                    $query = "INSERT INTO orders SET username=:username,created=:created";
+                    $order_query = "INSERT INTO orders SET username=:username,created=:created";
                     // prepare query for execution
-                    $stmt = $con->prepare($query);
+                    $order_stmt = $con->prepare($order_query);
                     // bind the parameters
-                    $stmt->bindParam(':username', $username);
-
+                    $order_stmt->bindParam(':username', $username);
                     // specify when this record was inserted to the database
                     $created = date('Y-m-d H:i:s');
-                    $stmt->bindParam(':created', $created);
+                    $order_stmt->bindParam(':created', $created);
                     // Execute the query
-                    if ($stmt->execute()) {
-                        echo "<div class='alert alert-success'>Record was saved.</div>";
+                    if ($order_stmt->execute()) {
+                        $lastInsertId = $con->lastInsertId(); // get the last inserted order ID
                         // Clear form fields
                         $username = "";
+
+                        // Insert into order_details table
+                        $product_name1 = $_POST['product1_name'];
+                        $product_name2 = $_POST['product2_name'];
+                        $product_name3 = $_POST['product3_name'];
+                        $quantity1 = $_POST['product1_quantity'];
+                        $quantity2 = $_POST['product2_quantity'];
+                        $quantity3 = $_POST['product3_quantity'];
+                        $created = date('Y-m-d H:i:s');
+
+                        // Get the product ID from the products table for each product separately
+                        $product1_query = "SELECT id FROM products WHERE name = :name1";
+                        $product1_stmt = $con->prepare($product1_query);
+                        $product1_stmt->bindParam(':name1', $product_name1);
+                        $product1_stmt->execute();
+                        $product1 = $product1_stmt->fetch(PDO::FETCH_ASSOC);
+
+                        $product2_query = "SELECT id FROM products WHERE name = :name2";
+                        $product2_stmt = $con->prepare($product2_query);
+                        $product2_stmt->bindParam(':name2', $product_name2);
+                        $product2_stmt->execute();
+                        $product2 = $product2_stmt->fetch(PDO::FETCH_ASSOC);
+
+                        $product3_query = "SELECT id FROM products WHERE name = :name3";
+                        $product3_stmt = $con->prepare($product3_query);
+                        $product3_stmt->bindParam(':name3', $product_name3);
+                        $product3_stmt->execute();
+                        $product3 = $product3_stmt->fetch(PDO::FETCH_ASSOC);
+
+
+                        // insert into order_details table
+                        $success = true;
+                        $od_query = "INSERT INTO order_detail SET order_id=:order_id, product_id=:product_id, quantity=:quantity, created=:created";
+                        // prepare query for execution
+                        $od_stmt = $con->prepare($od_query);
+                        // bind the parameters for product 1
+                        if (!empty($product_name1)) {
+                            $od_stmt->bindParam(':order_id', $lastInsertId);
+                            $od_stmt->bindParam(':product_id', $product['id']);
+                            $od_stmt->bindParam(':quantity', $quantity1);
+                            $od_stmt->bindParam(':created', $created);
+                        }
+
+                        // bind the parameters for product 2
+                        if (!empty($product_name2)) {
+                            $od_stmt->bindParam(':order_id', $lastInsertId);
+                            $od_stmt->bindParam(':product_id', $product['id']);
+                            $od_stmt->bindParam(':quantity', $quantity2);
+                            $od_stmt->bindParam(':created', $created);
+                        }
+
+                        // bind the parameters for product 3
+                        if (!empty($product_name3)) {
+                            $od_stmt->bindParam(':order_id', $lastInsertId);
+                            $od_stmt->bindParam(':product_id', $product['id']);
+                            $od_stmt->bindParam(':quantity', $quantity3);
+                            $od_stmt->bindParam(':created', $created);
+                        }
+
+                        $od_stmt->execute();
+                        $product = $od_stmt->fetch(PDO::FETCH_ASSOC);
+
+
+                        $error = $od_stmt->errorInfo();
+                        if (!$od_stmt->execute()) {
+                            $success = false;
+                            $error = $od_stmt->errorInfo();
+                            if ($error[0] != "00000") {
+                                echo "<div class='alert alert-danger'>Error: " . $error[2] . "</div>";
+                            }
+                        }
                     } else {
                         echo "<div class='alert alert-danger'>Unable to save record.</div>";
+                    }
+                    if ($success) {
+                        echo "<div class='alert alert-success'>Record was saved.</div>";
                     }
                 }
             }
@@ -94,11 +203,6 @@ if (!isset($_SESSION["username"])) {
                 die('ERROR: ' . $exception->getMessage());
             }
         }
-
-
-
-
-
         ?>
 
 
@@ -113,82 +217,27 @@ if (!isset($_SESSION["username"])) {
                         // include database connection
                         include 'config/database.php';
 
-                        // select all categories
+                        // select all customers
                         $query = "SELECT username FROM customers";
                         $stmt = $con->prepare($query);
                         $stmt->execute();
 
-                        // fetch the category list
+                        // fetch the customer list
                         $customers = $stmt->fetchAll(PDO::FETCH_COLUMN);
                         ?>
                         <select name='username' class="form-control">
                             <option value=''>--Select customer--</option>
-                            <?php foreach ($customers as $customers) { ?>
-                                <option value="<?php echo $customers; ?>"><?php echo $customers; ?></option>
-                            <?php } ?>
+                            <?php foreach ($customers as $customer) : ?>
+                                <option value='<?php echo $customer; ?>'><?php echo $customer; ?></option>
+                            <?php endforeach; ?>
                         </select>
-                        <?php if (isset($name_err)) { ?><span class="text-danger"><?php echo $name_err; ?></span><?php } ?>
+                        <?php if (isset($username_err)) { ?><span class="text-danger"><?php echo $username_err; ?></span><?php } ?>
                     </td>
                 </tr>
                 <tr>
                     <td>Product 1</td>
                     <td>
                         <?php
-                        // include database connection
-                        include 'config/database.php';
-
-                        // select all categories
-                        $query = "SELECT name FROM products";
-                        $stmt = $con->prepare($query);
-                        $stmt->execute();
-
-                        // fetch the category list
-                        $products = $stmt->fetchAll(PDO::FETCH_COLUMN);
-                        ?>
-                        <select name='name' class="form-control">
-                            <option value=''>--Select product--</option>
-                            <?php foreach ($products as $products) { ?>
-                                <option value="<?php echo $products; ?>"><?php echo $products; ?></option>
-                            <?php } ?>
-                        </select>
-                        <?php if (isset($product_name_err)) { ?><span class="text-danger"><?php echo $product_name_err; ?></span><?php } ?>
-                        <input type="number" name="quantity" class="form-control" placeholder="Enter quantity" min="1" required>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>Product 2</td>
-                    <td>
-                        <?php
-                        // include database connection
-                        include 'config/database.php';
-
-                        // select all categories
-                        $query = "SELECT name FROM products";
-                        $stmt = $con->prepare($query);
-                        $stmt->execute();
-
-                        // fetch the category list
-                        $products = $stmt->fetchAll(PDO::FETCH_COLUMN);
-                        ?>
-                        <select name='name' class="form-control">
-                            <option value=''>--Select product--</option>
-                            <?php foreach ($products as $products) { ?>
-                                <option value="<?php echo $products; ?>"><?php echo $products; ?></option>
-                            <?php } ?>
-                        </select>
-                        <?php if (isset($product_name_err)) { ?><span class="text-danger"><?php echo $product_name_err; ?></span><?php } ?>
-                        <input type="number" name="quantity" class="form-control" placeholder="Enter quantity" min="1" required>
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>Product 3</td>
-                    <td>
-                        <?php
-                        // include database connection
-                        include 'config/database.php';
-
                         // select all products
                         $query = "SELECT name FROM products";
                         $stmt = $con->prepare($query);
@@ -197,28 +246,61 @@ if (!isset($_SESSION["username"])) {
                         // fetch the product list
                         $products = $stmt->fetchAll(PDO::FETCH_COLUMN);
                         ?>
-                        <select name='name' class="form-control">
+                        <select name='product1_name' class="form-control">
                             <option value=''>--Select product--</option>
-                            <?php foreach ($products as $product) { ?>
-                                <option value="<?php echo $product; ?>"><?php echo $product; ?></option>
-                            <?php } ?>
+                            <?php foreach ($products as $product_name1) : ?>
+                                <option value='<?php echo $product_name1; ?>'><?php echo $product_name1; ?></option>
+                            <?php endforeach; ?>
                         </select>
-                        <?php if (isset($product_name_err)) { ?><span class="text-danger"><?php echo $product_name_err; ?></span><?php } ?>
-                        <input type="number" name="quantity" class="form-control" placeholder="Enter quantity" min="1" required>
+                        <?php if (isset($product1_name_err)) { ?><span class="text-danger"><?php echo $product1_name_err; ?></span><?php } ?>
+                    </td>
+                    <td>
+                        <input type='number' name='product1_quantity' class='form-control'>
                     </td>
                 </tr>
+                <tr>
+                    <td>Product 2</td>
+                    <td>
+                        <select name='product2_name' class="form-control">
+                            <option value=''>--Select product--</option>
+                            <?php foreach ($products as $product_name2) : ?>
+                                <option value='<?php echo $product_name2; ?>'><?php echo $product_name2; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                        <?php if (isset($product2_name_err)) { ?><span class="text-danger"><?php echo $product2_name_err; ?></span><?php } ?>
+                    </td>
+                    <td>
+                        <input type='number' name='product2_quantity' class='form-control'>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Product 3</td>
+                    <td>
+                        <?php
+                        // select all products
+                        $query = "SELECT name FROM products";
+                        $stmt = $con->prepare($query);
+                        $stmt->execute();
 
-
-
-
-                <td></td>
-                <td>
-                    <input type='submit' value='Save' class='btn btn-primary' />
-                    <a href='product_read.php' class='btn btn-danger'>Back to read products</a>
-                </td>
+                        // fetch the product list
+                        $products = $stmt->fetchAll(PDO::FETCH_COLUMN);
+                        ?>
+                        <select name='product3_name' class="form-control">
+                            <option value=''>--Select product--</option>
+                            <?php foreach ($products as $product_name3) : ?>
+                                <option value='<?php echo $product_name3; ?>'><?php echo $product_name3; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                        <?php if (isset($product3_name_err)) { ?><span class="text-danger"><?php echo $product3_name_err; ?></span><?php } ?>
+                    </td>
+                    <td>
+                        <input type='number' name='product3_quantity' class='form-control'>
+                    </td>
                 </tr>
             </table>
+            <input type='submit' value='Create Order' class='btn btn-primary'>
         </form>
+
 
 
     </div>
