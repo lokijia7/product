4a9f1b26880678d05b9a38573af129435dd881b4
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-04-25 07:37:31 +0800

    chg title

--- home.php
+++ home.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION["username"])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Store Background Management System</title>
+    <title>Typo-Home page</title>
     <link rel="stylesheet" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <!-- Latest compiled and minified Bootstrap CSS (Apply your Bootstrap here) -->
