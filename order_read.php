9ab9d7e9c03b416a4b41ccb20969033564ab6003
LOK JIA QI <jiaqi213006@dasein.edu.my>
2023-05-16 08:10:10 +0800

    del del button

--- order_read.php
+++ order_read.php
@@ -98,7 +98,6 @@ if (!isset($_SESSION["username"])) {
                 // read one record
                 echo "<div class='button-group1'>";
                 echo "<a href='order_read_one.php?order_id={$order_id}' class='btn btn-info btn-sm d-inline'>Read</a>&nbsp;";
-                echo "<a href='#' onclick='delete_user({$id});'  class='btn btn-danger btn-sm d-inline'>Delete</a>";
                 echo "</div>";
                 echo "</td>";
                 echo "</tr>";
